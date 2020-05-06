7f96728cbaa5eecba42a6b0b6972e561bc7bbc36
menglung <78449598@qq.com>
2020-05-06 21:08:14 +0800

    commit 7

--- src/main/java/com/home/controller/GitController.java
+++ src/main/java/com/home/controller/GitController.java
@@ -7,9 +7,6 @@ public class GitController {
     public static void main(String[] args) {
         System.out.println("is1");
         System.out.println("is2");
-        System.out.println("is3");
-        System.out.println("is4");
-        System.out.println("is5");
-        System.out.println("is6");
+        System.out.println("is7");
     }
 }
