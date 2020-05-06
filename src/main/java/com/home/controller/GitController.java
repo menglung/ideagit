1678c91a3db2c15ca96358fabdb141c223d47513
menglung <78449598@qq.com>
2020-05-06 20:17:13 +0800

    Initial commit

---
+++ src/main/java/com/home/controller/GitController.java
@@ -0,0 +1,10 @@
+package com.home.controller;
+
+import sun.misc.Contended;
+
+@Contended
+public class GitController {
+    public static void main(String[] args) {
+        System.out.println("is");
+    }
+}
