ae25b9b506f152c5a9c8e92f6d06001a62ad9af8
“menglung” <78449598@qq.com>
2020-05-15 23:15:17 +0800

    1,2,3,4,5

--- src/main/java/com/home/controller/GitController.java
+++ src/main/java/com/home/controller/GitController.java
@@ -7,9 +7,9 @@ public class GitController {
     public static void main(String[] args) {
         System.out.println("is1");
         System.out.println("is2");
-        System.out.println("is7");
-        System.out.println("is8");
-        System.out.println("is9");
-        System.out.println("is10");
+        System.out.println("is3");
+        System.out.println("is4");
+        System.out.println("is5");
+        System.out.println("is6");
     }
 }
